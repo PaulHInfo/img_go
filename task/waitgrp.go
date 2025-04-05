4d3ccde7bccc5d7b769bd47e2fd230846dd19334
Paul <paulhuwiler.info@gmail.com>
2025-04-05 17:50:04 +0200

    new src

--- task/waitgrp.go
+++ task/waitgrp.go
@@ -1,19 +1,46 @@
 package task
 
-import "imgproc/filter"
+import (
+	"fmt"
+	"imgproc/filter"
+	"path"
+	"path/filepath"
+	"sync"
+)
 
 type WaitGrpTask struct {
 	dirCtx
 	Filter filter.Filter
 }
 
-func NewWaitGrpTask(srcDir, dstDir string, Filter filter.Filter) Tasker {
-	return &WaitGrpTask{
+func NewWaitGrpTask(srcDir, dstDir string, filter filter.Filter) Tasker {
+	return WaitGrpTask{
 		Filter: filter,
 		dirCtx: dirCtx{
-			srcDir: srcDir,
-			DstDir: DstDir,
-			files:  BuildFileList(srcDir),
+			SrcDir: srcDir,
+			DstDir: dstDir,
+			files:  buildFileList(srcDir),
 		},
 	}
+}
+
+func (w *WaitGrpTask) Process() error {
+	var wg sync.WaitGroup
+	size := len(w.files)
+	for i, f := range w.files {
+		filename := filepath.Base(f)
+		dst := path.Join(w.DstDir, filename)
+		wg.Add(1)
+		go w.applyFilter(f, dst, &wg, i+1, size)
+	}
+
+	wg.Wait()
+	fmt.Println("Done processing files!")
+	return nil
+}
+
+func (w *WaitGrpTask) applyFilter(src string, dst string, wg *sync.WaitGroup, i int, size int) {
+	w.Filter.Process(src, dst)
+	fmt.Printf("Processed [%d/%d] %v => %v\n", i, size, src, dst)
+	wg.Done()
 }
